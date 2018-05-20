6dbd2bcc45fc390ff4d2f0e7bfa8d8344be69934
ryu-g <em.ryu.g@gmail.com>
2018-05-20 20:04:43 +0900

    dummy

--- token.js
+++ token.js
@@ -1,7 +1,7 @@
 module.exports.token = {
-  consumer_key: 'GYVNA8aW0hI3S3jFc0YeUBYc0',
-  consumer_secret: '1Qa72FM9DIDu55H10t8CJnWzX61TLghgoFYbI0A8yjCT9thmGK',
-  access_token_key: '2958581048-KfSTb2DBnXSG3ILFQuxTQAVQVXcHG2a5MXu78w7',
-  access_token_secret: 'fMDlYYl2vFkAMpMpF8EEBR0W0YNobeuVYVi148o9owUkD'
+  consumer_key: 'X',
+  consumer_secret: 'X',
+  access_token_key: 'X-X',
+  access_token_secret: 'X'
 }
 
